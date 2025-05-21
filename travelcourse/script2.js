bdcb566c4712759ce741ad1691fd01303a2daee5
Kim-961204 <ddj03285@naver.com>
2025-05-21 12:20:08 +0900

    Add files via upload

---
+++ travelcourse/script2.js
@@ -0,0 +1,122 @@
+//팝업 띄우기(파리)
+function openPop() {
+    document.getElementById("popup_layer").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop() {
+    document.getElementById("popup_layer").style.display = "none";
+}
+
+//팝업 띄우기(하와이)
+function openPop1() {
+    document.getElementById("popup_layer1").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop1() {
+    document.getElementById("popup_layer1").style.display = "none";
+}
+
+//팝업 띄우기(바르셀로나)
+function openPop2() {
+    document.getElementById("popup_layer2").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop2() {
+    document.getElementById("popup_layer2").style.display = "none";
+}
+//팝업 띄우기(오사카)
+function openPop3() {
+    document.getElementById("popup_layer3").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop3() {
+    document.getElementById("popup_layer3").style.display = "none";
+}
+//팝업 띄우기(니스)
+function openPop4() {
+    document.getElementById("popup_layer4").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop4() {
+    document.getElementById("popup_layer4").style.display = "none";
+}
+//팝업 띄우기(괌)
+function openPop5() {
+    document.getElementById("popup_layer5").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop5() {
+    document.getElementById("popup_layer5").style.display = "none";
+}
+//팝업 띄우기(마드리드)
+function openPop6() {
+    document.getElementById("popup_layer6").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop6() {
+    document.getElementById("popup_layer6").style.display = "none";
+}
+//팝업 띄우기(도쿄)
+function openPop7() {
+    document.getElementById("popup_layer7").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop7() {
+    document.getElementById("popup_layer7").style.display = "none";
+}
+//팝업 띄우기(후쿠오카)
+function openPop8() {
+    document.getElementById("popup_layer8").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop8() {
+    document.getElementById("popup_layer8").style.display = "none";
+}
+//팝업 띄우기(사이판)
+function openPop9() {
+    document.getElementById("popup_layer9").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop9() {
+    document.getElementById("popup_layer9").style.display = "none";
+}
+//팝업 띄우기(로스엔젤레스)
+function openPop10() {
+    document.getElementById("popup_layer10").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop10() {
+    document.getElementById("popup_layer10").style.display = "none";
+}
+//팝업 띄우기(뉴욕)
+function openPop11() {
+    document.getElementById("popup_layer11").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop11() {
+    document.getElementById("popup_layer11").style.display = "none";
+}
